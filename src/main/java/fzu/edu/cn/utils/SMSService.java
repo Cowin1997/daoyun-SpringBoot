07e034a6776ad231be3c7b501f9a5f3a1902ffe1
Cowin1997 <36791203+Cowin1997@users.noreply.github.com>
2021-03-27 13:30:36 +0800

    no message

---
+++ src/main/java/fzu/edu/cn/utils/SMSService.java
@@ -0,0 +1,40 @@
+package fzu.edu.cn.utils;
+
+
+
+
+
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Map;
+
+public class SMSService {
+    /** 产品密钥ID，产品标识 */
+    private final static String SECRETID = "7cfe56fae3d93403e5a55414d29811cd";
+    /** 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露 */
+    private final static String SECRETKEY = "4e9b080e371e651487dc2ba03704b059";
+    /** 本机认证服务身份证实人认证在线检测接口地址 */
+    private final static String API_URL = "https://sms.dun.163.com/v2/sendsms";
+    /** 实例化HttpClient，发送http请求使用，可根据需要自行调参 */
+    //private static HttpClient httpClient = HttpClient4Utils.createHttpClient(100, 100, 2000, 2000, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
